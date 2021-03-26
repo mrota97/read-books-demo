b71632d0ab33d2a3cafb77c4cfb86c6d6bd70e45
Matt Rota <mrota97@gmail.com>
2021-03-26 19:38:01 -0400

    rough draft with placeholders, no interaction

--- src/App.js
+++ src/App.js
@@ -1,28 +1,154 @@
-import './App.scss';
+import './styles/App.scss';
+import React from 'react';
+
+// icons and placeholders
+import { run as runHolder } from 'holderjs/holder'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSearch, faBars, faAngleDown, faStar } from "@fortawesome/free-solid-svg-icons"
+
+function BookEntry() {
+  return (
+    <div className="book-entry-container">
+      <div className="book-entry-box">
+        {/* main */}
+        
+        <div className="book-entry-main">
+          <div className="book-cover-container">
+            <div>
+              <img src="holder.js/150x226?auto=yes" alt="placeholder"/>
+            </div>
+          </div>
+          <div className="book-details">
+            <h1>Title</h1>
+            <h3>Author</h3>
+            <div>
+              <div>
+                <FontAwesomeIcon icon={faStar} color="green" />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+              </div>
+              <h4>2,000,000 votes</h4>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ultricies nisl at urna fringilla aliquet. Donec dictum. </p>
+          </div>
+        </div>
+
+        {/* footer */}
+        <div className="book-entry-footer">
+          <span>test</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function BookCarouselItem() {
+  return (
+    <div className="book-carousel-item">
+      <div className="book-cover-container">
+        <div>
+          <img src="holder.js/242x365?auto=yes" alt="placeholder"/>
+        </div>
+      </div>
+      <div className="book-details">
+        <h1>BIG MAGIC: Creative Living Beyond Fear</h1>
+        <h3>by Author</h3>
+        <div className="rating">
+          <div>
+            <FontAwesomeIcon icon={faStar} color="yellow" />
+            <FontAwesomeIcon icon={faStar} color="yellow" />
+            <FontAwesomeIcon icon={faStar} color="yellow" />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+          </div>
+          <h4>2,000,000 votes</h4>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ultricies nisl at urna fringilla aliquet. Donec dictum. </p>
+        <button>
+          See This Book
+        </button>
+      </div>
+    </div>
+  )
+}
+
+function AuthorListEntry({ name }) {
+  return (
+    <div className="author">
+      <div className="author-pfp">
+        <div>
+          <img src="holder.js/30x30" alt="author profile icon" />
+        </div>
+      </div>
+      <h4>{name}</h4>
+    </div>
+  )
+}
+
+function BookListEntry({ name, author }) {
+  return (
+    <div className="book">
+      <div className="book-cover">
+        <div>
+          <img src="holder.js/50x75" alt="best books cover" />
+        </div>
+      </div>
+      <div className="book-info">
+        <h4>{name}</h4>
+        <h5>by {author}</h5>
+      </div>
+    </div>
+  )
+}
 
 function App() {
+  React.useEffect(() => { runHolder() }, [])
   return (
     <div className="App">
       {/* navbar container */}
       <header className="App-header">
-        <ul className="header-left">
-          <li>Browse Catagory</li>
-          <li>|</li>
-          <li>Search Book</li>
-        </ul>
+        <div className="header-left">
+          <div>
+            <h3>Browse Catagory</h3>
+            <FontAwesomeIcon icon={faAngleDown} />
+          </div>
+          <div>|</div>
+          <div>
+            <FontAwesomeIcon icon={faSearch} />
+            <h3><span style={{ color: "lightgray" }}>Search Book</span></h3>
+          </div>
+        </div>
 
-        <p>readbooks</p>
+        <div className="header-brand">
+          <h1><span style={{ fontWeight: "normal"}}>read</span>books</h1>
+        </div>
+        
 
-        <ul className="header-right">
-          <li>icon</li>
-          <li>|</li>
-          <li>menu</li>
-        </ul>
+        <div className="header-right">
+          <div>
+            <div>
+              <img src="holder.js/30x30" alt="user profile" />
+            </div>
+          </div>
+          <div>|</div>
+          <div>
+            <FontAwesomeIcon icon={faBars} />
+            <h3>Menu</h3>
+          </div>
+        </div>
 
       </header>
 
       {/* carousel container */}
-      <div className="App-carousel"></div>
+      <div className="App-carousel">
+        {Array(10).fill(0).map(v => (
+          <div className="slide">
+            <BookCarouselItem />
+          </div>
+        ))}
+      </div>
 
       {/* main content container */}
       <div className="App-main">
@@ -30,28 +156,28 @@ function App() {
         {/* sidebar */}
         <div className="sidebar">
           {/* author list */}
-          <section>
+          <section className="best-authors">
             <h2>Author of the Week</h2>
             <ul>
-              <li>Sebastian Jeremy</li>
-              <li>Jonathan Doe</li>
-              <li>Angeline Summer</li>
-              <li>Noah Jones</li>
-              <li>Tommy Adam</li>
-              <li>Ian Cassandra</li>
+              <li><AuthorListEntry name={"Sebastian Jeremy"} /></li>
+              <li><AuthorListEntry name={"Jonathan Doe"} /></li>
+              <li><AuthorListEntry name={"Angeline Summer"} /></li>
+              <li><AuthorListEntry name={"Noah Jones"} /></li>
+              <li><AuthorListEntry name={"Tommy Adam"} /></li>
+              <li><AuthorListEntry name={"Ian Cassandra"} /></li>
             </ul>
           </section>
 
           {/* book list */}
-          <section>
+          <section className="best-books">
             <h2>Books of the Year</h2>
             <ul>
-              <li>Big Magic: Create living Beyond Fear</li>
-              <li>Big Magic: Create living Beyond Fear</li>
-              <li>Big Magic: Create living Beyond Fear</li>
-              <li>Big Magic: Create living Beyond Fear</li>
-              <li>Big Magic: Create living Beyond Fear</li>
-              <li>Big Magic: Create living Beyond Fear</li>
+              <li><BookListEntry name={"Big Magic: Create living Beyond Fear"} author={"Elizabeth Gilbert"} /></li>
+              <li><BookListEntry name={"Big Magic: Create living Beyond Fear"} author={"Elizabeth Gilbert"} /></li>
+              <li><BookListEntry name={"Big Magic: Create living Beyond Fear"} author={"Elizabeth Gilbert"} /></li>
+              <li><BookListEntry name={"Big Magic: Create living Beyond Fear"} author={"Elizabeth Gilbert"} /></li>
+              <li><BookListEntry name={"Big Magic: Create living Beyond Fear"} author={"Elizabeth Gilbert"} /></li>
+              <li><BookListEntry name={"Big Magic: Create living Beyond Fear"} author={"Elizabeth Gilbert"} /></li>
             </ul>
           </section>
         </div>
@@ -61,7 +187,7 @@ function App() {
           
           {/* genre tabs */}
           <section className="genre-toolbar">
-            <h3 className="genre-header">Popular by Genre</h3>
+            <h2>Popular by Genre</h2>
             <ul className="genre-list">
               <li>All Genres</li>
               <li>Business</li>
@@ -73,8 +199,9 @@ function App() {
           </section>
 
           {/* book list grid */}
-          <div className="book-grid"></div>
-
+          <div className="book-grid">
+            {Array(10).fill(0).map(v => <BookEntry />)}
+          </div>
         </div>
       </div>
     </div>
